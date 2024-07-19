dbac7e78ec6a330501e2ed24e57cf626130e1258
SudenazYazici <sudenyazici@gmail.com>
2024-07-19 12:57:25 +0300

    Admin can add movie and theatre now

--- src/Components/Admin.js
+++ src/Components/Admin.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
-import { Register } from "./Register";
 import { useAuth } from "./auth";
+import { useForm } from "react-hook-form";
+import axios from "axios";
 
 export const Admin = () => {
     const auth = useAuth();
@@ -13,10 +14,104 @@ export const Admin = () => {
         }
     }, [auth.user])
 
+    const [isError, setIsError] = useState(false);
+
+    const {
+        register: registerMovie,
+        handleSubmit: handleSubmitMovie,
+        formState: { errors: movieErrors },
+    } = useForm();
+
+    const {
+        register: registerTheatre,
+        handleSubmit: handleSubmitTheatre,
+        formState: { errors: theatreErrors },
+    } = useForm();
+
+    const onMovieSubmit = (data) => {
+        axios.post('https://localhost:7030/api/Movie', data)
+            .then(response => {
+                console.log('Registration successful', response.data);
+                setIsError(false);
+            })
+            .catch(error => {
+                console.error('There was an error adding movie!', error);
+                setIsError(true);
+            });
+    };
+
+    const onTheatreSubmit = (data) => {
+        axios.post('https://localhost:7030/api/Cinema', data)
+            .then(response => {
+                console.log('Registration successful', response.data);
+                setIsError(false);
+            })
+            .catch(error => {
+                console.error('There was an error adding theatre!', error);
+                setIsError(true);
+            });
+    };
+
     return(
         <div>
             {isAdmin ? (
-                <div>Admin page</div>
+                <div>
+                    
+                    <div className="max-w-md mx-auto">
+                        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmitMovie(onMovieSubmit)}>
+                            <div className="text-black font-bold text-center">Add Movie</div>
+                            <div className="mb-4 form-control">
+                                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="movie-name">
+                                    Movie name
+                                </label>
+                                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="movie-name" type="text" placeholder="Movie name" {...registerMovie("name", { required: true })}/>
+                            </div>
+                            <div className="mb-4 form-control">
+                                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="movie-details">
+                                    Movie Details
+                                </label>
+                                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="movie-details" type="text" placeholder="Movie details" {...registerMovie("details", { required: true })}/>
+                            </div>
+                            <div className="flex items-center justify-between form-control">
+                                <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+                                    Add Movie
+                                </button>
+                            </div>
+                            {isError && <p className="mt-5 text-red-500 text-xs italic">There was an error adding movie! Please try again.</p>}
+                        </form>
+                    </div>
+                    
+                    <div className="max-w-md mx-auto">
+                        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmitTheatre(onTheatreSubmit)}>
+                            <div className="text-black font-bold text-center">Add Movie Theatre</div>
+                            <div className="mb-4 form-control">
+                                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="theatre-name">
+                                    Theatre name
+                                </label>
+                                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="theatre-name" type="text" placeholder="Theatre name" {...registerTheatre("name", { required: true })}/>
+                            </div>
+                            <div className="mb-4 form-control">
+                                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="city">
+                                    City
+                                </label>
+                                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="city" type="text" placeholder="City" {...registerTheatre("city", { required: true })}/>
+                            </div>
+                            <div className="mb-4 form-control">
+                                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="address">
+                                    Address
+                                </label>
+                                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="address" type="text" placeholder="Address" {...registerTheatre("address", { required: true })}/>
+                            </div>
+                            <div className="flex items-center justify-between form-control">
+                                <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+                                    Add Theatre
+                                </button>
+                            </div>
+                            {isError && <p className="mt-5 text-red-500 text-xs italic">There was an error adding theatre! Please try again.</p>}
+                        </form>
+                    </div>
+                </div>
+                
             ) : (
                 <div>You do not have access to this page.</div>
             )}
