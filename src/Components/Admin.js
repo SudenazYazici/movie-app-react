89909eec56e2fd30783e4e7e81ec3cecbae8dcda
SudenazYazici <sudenyazici@gmail.com>
2024-07-19 15:02:58 +0300

    Admin can delete movies and theatres now

--- src/Components/Admin.js
+++ src/Components/Admin.js
@@ -51,65 +51,176 @@ export const Admin = () => {
                 setIsError(true);
             });
     };
+    const [theatres, setTheatres] = useState([]);
+    const [movies, setMovies] = useState([]);
+      useEffect(() => {
+
+        axios.get('https://localhost:7030/api/Cinema')
+        .then(response => {
+            setTheatres(response.data);
+            console.log(response.data);
+        })
+        .catch(error => {
+            console.error('There was an error fetching the data!', error);
+        })
+        axios.get('https://localhost:7030/api/Movie')
+        .then(response => {
+            setMovies(response.data);
+            console.log(response.data);
+        })
+        .catch(error => {
+            console.error('There was an error fetching the data!', error);
+        })
+      }, []);
+
+    const onDeleteMovie = (movieId) => {
+        const userConfirmed = window.confirm("Are you sure you want to delete this movie?");
+
+        if(userConfirmed) {
+            axios.delete(`https://localhost:7030/api/Movie/${movieId}`)
+            .then(response => {
+                console.log('Deletion successful', response.data);
+                window.location.reload();
+            })
+            .catch(error => {
+                console.error('There was an error deleting!', error);
+            });
+        }
+        
+    };
+
+    const onDeleteCinema = (cinemaId) => {
+        const userConfirmed = window.confirm("Are you sure you want to delete this movie theatre?");
+
+        if(userConfirmed) {
+            axios.delete(`https://localhost:7030/api/Cinema/${cinemaId}`)
+            .then(response => {
+                console.log('Deletion successful', response.data);
+                window.location.reload();
+            })
+            .catch(error => {
+                console.error('There was an error deleting!', error);
+            });
+        }
+        
+    };
 
     return(
         <div>
             {isAdmin ? (
                 <div>
-                    
-                    <div className="max-w-md mx-auto">
-                        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmitMovie(onMovieSubmit)}>
-                            <div className="text-black font-bold text-center">Add Movie</div>
-                            <div className="mb-4 form-control">
-                                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="movie-name">
-                                    Movie name
-                                </label>
-                                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="movie-name" type="text" placeholder="Movie name" {...registerMovie("name", { required: true })}/>
-                            </div>
-                            <div className="mb-4 form-control">
-                                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="movie-details">
-                                    Movie Details
-                                </label>
-                                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="movie-details" type="text" placeholder="Movie details" {...registerMovie("details", { required: true })}/>
-                            </div>
-                            <div className="flex items-center justify-between form-control">
-                                <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
-                                    Add Movie
-                                </button>
-                            </div>
-                            {isError && <p className="mt-5 text-red-500 text-xs italic">There was an error adding movie! Please try again.</p>}
-                        </form>
+                    <div className="flex items-center">
+                        <div className="max-w-md mx-auto w-96">
+                            <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmitMovie(onMovieSubmit)}>
+                                <div className="text-black font-bold text-center">Add Movie</div>
+                                <div className="mb-4 form-control">
+                                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="movie-name">
+                                        Movie name
+                                    </label>
+                                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="movie-name" type="text" placeholder="Movie name" {...registerMovie("name", { required: true })}/>
+                                </div>
+                                <div className="mb-4 form-control">
+                                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="movie-details">
+                                        Movie Details
+                                    </label>
+                                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="movie-details" type="text" placeholder="Movie details" {...registerMovie("details", { required: true })}/>
+                                </div>
+                                <div className="flex items-center justify-between form-control">
+                                    <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+                                        Add Movie
+                                    </button>
+                                </div>
+                                {isError && <p className="mt-5 text-red-500 text-xs italic">There was an error adding movie! Please try again.</p>}
+                            </form>
+                        </div>
+                        
+                        <div className="max-w-md mx-auto w-96">
+                            <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmitTheatre(onTheatreSubmit)}>
+                                <div className="text-black font-bold text-center">Add Movie Theatre</div>
+                                <div className="mb-4 form-control">
+                                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="theatre-name">
+                                        Theatre name
+                                    </label>
+                                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="theatre-name" type="text" placeholder="Theatre name" {...registerTheatre("name", { required: true })}/>
+                                </div>
+                                <div className="mb-4 form-control">
+                                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="city">
+                                        City
+                                    </label>
+                                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="city" type="text" placeholder="City" {...registerTheatre("city", { required: true })}/>
+                                </div>
+                                <div className="mb-4 form-control">
+                                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="address">
+                                        Address
+                                    </label>
+                                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="address" type="text" placeholder="Address" {...registerTheatre("address", { required: true })}/>
+                                </div>
+                                <div className="flex items-center justify-between form-control">
+                                    <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+                                        Add Theatre
+                                    </button>
+                                </div>
+                                {isError && <p className="mt-5 text-red-500 text-xs italic">There was an error adding theatre! Please try again.</p>}
+                            </form>
+                        </div>
+                    </div>
+                    <div className="flex items-center">
+                        <div className="relative overflow-scroll rounded mt-10 max-w-md mx-auto w-96 h-64">
+                            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                    <tr>
+                                        <th scope="col" className="px-6 py-3">
+                                            All Movies
+                                        </th>
+                                        <th scope="col" className="px-6 py-3">
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {movies.map(movie => (
+                                        <tr key={movie.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                {movie.name}
+                                            </th>
+                                            <td className="px-6 py-4">
+                                            <button onClick={() => onDeleteMovie(movie.id)} className="inline-block bg-red-700 rounded hover:border-gray-200 text-white hover:bg-red-900 py-1 px-3">
+                                                   Delete
+                                            </button>
+                                            </td>
+                                        </tr>
+                                    ))} 
+                                </tbody>
+                             </table>
+                         </div>
+                        <div className="relative overflow-scroll rounded mt-10 max-w-md mx-auto w-96 h-64">
+                            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                    <tr>
+                                        <th scope="col" className="px-6 py-3">
+                                            All Movie Theatres
+                                        </th>
+                                        <th scope="col" className="px-6 py-3">
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {theatres.map(theatre => (
+                                        <tr key={theatre.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                {theatre.name}
+                                            </th>
+                                            <td className="px-6 py-4">
+                                            <button onClick={() => onDeleteCinema(theatre.id)} className="inline-block bg-red-700 rounded hover:border-gray-200 text-white hover:bg-red-900 py-1 px-3">
+                                                    Delete
+                                            </button>
+                                            </td>
+                                        </tr>
+                                    ))} 
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                     
-                    <div className="max-w-md mx-auto">
-                        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmitTheatre(onTheatreSubmit)}>
-                            <div className="text-black font-bold text-center">Add Movie Theatre</div>
-                            <div className="mb-4 form-control">
-                                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="theatre-name">
-                                    Theatre name
-                                </label>
-                                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="theatre-name" type="text" placeholder="Theatre name" {...registerTheatre("name", { required: true })}/>
-                            </div>
-                            <div className="mb-4 form-control">
-                                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="city">
-                                    City
-                                </label>
-                                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="city" type="text" placeholder="City" {...registerTheatre("city", { required: true })}/>
-                            </div>
-                            <div className="mb-4 form-control">
-                                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="address">
-                                    Address
-                                </label>
-                                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="address" type="text" placeholder="Address" {...registerTheatre("address", { required: true })}/>
-                            </div>
-                            <div className="flex items-center justify-between form-control">
-                                <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
-                                    Add Theatre
-                                </button>
-                            </div>
-                            {isError && <p className="mt-5 text-red-500 text-xs italic">There was an error adding theatre! Please try again.</p>}
-                        </form>
-                    </div>
                 </div>
                 
             ) : (
