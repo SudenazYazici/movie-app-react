7e8371a4f14d91ac8bf2568dadba374918410ab5
SudenazYazici <sudenyazici@gmail.com>
2024-07-19 11:38:26 +0300

    added auth to admin page

--- src/Components/Admin.js
+++ src/Components/Admin.js
@@ -1,7 +1,14 @@
 import { Register } from "./Register";
+import { useAuth } from "./auth";
 
 export const Admin = () => {
+    const auth = useAuth();
+
     return(
-        <div>Admin page</div>
+        <div>
+            {auth.user.role == "admin" && (
+                <div>Admin page</div>
+            )}
+        </div>
     );
 }
