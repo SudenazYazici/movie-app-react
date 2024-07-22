6b4315ed94dec4170d7e7ae31d885652be1f218e
SudenazYazici <sudenyazici@gmail.com>
2024-07-22 11:28:02 +0300

    navbar made responsive

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,10 +6,13 @@ import { useState } from "react";
 import { CgLogOut } from "react-icons/cg";
 import { SearchBar } from "./SearchBar";
 import { useEffect } from "react";
+import { FaBars } from "react-icons/fa";
 
 export const Navbar = () => {
     const [dropdownOpen, setDropdownOpen] = useState(false)
     const [isAdmin, setIsAdmin] = useState(false);
+    const [menuOpen, setMenuOpen ] = useState(window.innerWidth < 769);
+    const [showModal, setShowModal ] = useState(false);
 
     const auth = useAuth();
     const userRole = localStorage.getItem('userRole');
@@ -21,106 +24,211 @@ export const Navbar = () => {
         }
     }, [auth.user])
 
+    useEffect(() => {
+        const handleResize = () => {
+            setMenuOpen(window.innerWidth < 769);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     const handleLogout = () => {
         auth.logout();
         setDropdownOpen(false);
         navigate('/');
     }
 
+    const toggleModal = () => {
+        setShowModal(!showModal);
+    };
+
+    const handleBarsIconClick = () => {
+        toggleModal();
+    };
+
     return(
         <>
-            <nav className="absolute w-full p-4 flex items-center justify-between z-50">
-                <div>
-                    <NavLink to='/'>
-                        <button className="inline-block rounded text-red-400 hover:bg-zinc-800 py-1 px-3 hover:text-white" onClick={() => setDropdownOpen(false)}>
-                            Home
-                        </button>
-                    </NavLink>
-                    <NavLink to='/movies'>
-                        <button className="inline-block rounded text-red-400 hover:bg-zinc-800 py-1 px-3 hover:text-white" onClick={() => setDropdownOpen(false)}>
-                            Movies
-                        </button>
-                    </NavLink>
-                    <NavLink to='/movie-theatres'>
-                        <button className="inline-block rounded text-red-400 hover:bg-zinc-800 py-1 px-3 hover:text-white" onClick={() => setDropdownOpen(false)}>
-                            Movie Theatres
-                        </button>
-                    </NavLink>
-                    <NavLink to='/book-tickets'>
-                        <button className="inline-block rounded text-red-400 hover:bg-zinc-800 py-1 px-3 hover:text-white" onClick={() => setDropdownOpen(false)}>
-                            Book Tickets
-                        </button>
-                    </NavLink>
-                    {auth.user && isAdmin ? (
-                        <NavLink to='/admin'>
-                            <button className="inline-block rounded text-red-400 hover:bg-zinc-800 py-1 px-3 hover:text-white" onClick={() => setDropdownOpen(false)}>
-                                Panel
-                            </button>
-                        </NavLink>
-                    ): (
-                        <div></div>
-                    )}
-                </div>
-                {!auth.user && (
-                    <div className="flex items-center">
-                        <SearchBar/>
-                        <NavLink to='/admin-register'>
-                            <button className="inline-block rounded text-red-400 hover:bg-zinc-800 py-1 px-3 hover:text-white" onClick={() => setDropdownOpen(false)}>
-                                Admin
-                            </button>
-                        </NavLink>
-                        <NavLink to='/login'>
-                            <button className="inline-block rounded text-red-400 hover:bg-zinc-800 py-1 px-3 hover:text-white" onClick={() => setDropdownOpen(false)}>
-                                Login
-                            </button>
-                        </NavLink>
-                        <NavLink to='/register'>
-                            <button className="inline-block bg-red-700 rounded hover:border-gray-200 text-white hover:bg-red-900 py-1 px-3">
-                                Sign up
-                            </button>
-                        </NavLink>
-                    </div>
-                )}
-                {auth.user && (
+            {!menuOpen ? ( 
+                // Laptop Navbar code
+                <nav className="absolute w-full p-4 flex items-center justify-between z-50">
                     <div>
-                        <div className="flex items-center">
-                            <SearchBar/>
-                            <button
-                                className="inline-block bg-gray-700 rounded hover:border-gray-200 text-white hover:bg-gray-900 py-2 px-4"
-                                onClick={() => setDropdownOpen(!dropdownOpen)}
-                            >
-                                <VscAccount />
+                        <NavLink to='/'>
+                            <button className="inline-block rounded text-red-400 hover:bg-zinc-800 py-1 px-3 hover:text-white" onClick={() => setDropdownOpen(false)}>
+                                Home
                             </button>
-                        </div>
-                        
-                        {dropdownOpen && (
-                            <div className="absolute right-0 mt-2 w-48 bg-white border border-gray-300 rounded-md shadow-lg">
-                                <NavLink to='/tickets'>
-                                    <button
-                                        className="rounded block w-full px-4 py-2 text-left text-sm text-gray-700 hover:bg-gray-100"
-                                        onClick={() => setDropdownOpen(false)}
-                                    >
-                                        <div>
-                                            My Tickets
-                                        </div>
-                                    </button>
-                                </NavLink>
-                                <button
-                                    className="rounded block w-full px-4 py-2 text-left text-sm text-gray-700 hover:bg-gray-100"
-                                    onClick={handleLogout}
-                                >
-                                    <div className="flex items-center">
-                                        <CgLogOut className="mr-2" />
-                                        Logout
-                                    </div>
-                                    
+                        </NavLink>
+                        <NavLink to='/movies'>
+                            <button className="inline-block rounded text-red-400 hover:bg-zinc-800 py-1 px-3 hover:text-white" onClick={() => setDropdownOpen(false)}>
+                                Movies
+                            </button>
+                        </NavLink>
+                        <NavLink to='/movie-theatres'>
+                            <button className="inline-block rounded text-red-400 hover:bg-zinc-800 py-1 px-3 hover:text-white" onClick={() => setDropdownOpen(false)}>
+                                Movie Theatres
+                            </button>
+                        </NavLink>
+                        <NavLink to='/book-tickets'>
+                            <button className="inline-block rounded text-red-400 hover:bg-zinc-800 py-1 px-3 hover:text-white" onClick={() => setDropdownOpen(false)}>
+                                Book Tickets
+                            </button>
+                        </NavLink>
+                        {auth.user && isAdmin ? (
+                            <NavLink to='/admin'>
+                                <button className="inline-block rounded text-red-400 hover:bg-zinc-800 py-1 px-3 hover:text-white" onClick={() => setDropdownOpen(false)}>
+                                    Panel
                                 </button>
-                            </div>
+                            </NavLink>
+                        ): (
+                            <div></div>
                         )}
                     </div>
-                )}
-            </nav>
+                    {!auth.user && (
+                        <div className="flex items-center">
+                            <SearchBar/>
+                            <NavLink to='/admin-register'>
+                                <button className="inline-block rounded text-red-400 hover:bg-zinc-800 py-1 px-3 hover:text-white" onClick={() => setDropdownOpen(false)}>
+                                    Admin
+                                </button>
+                            </NavLink>
+                            <NavLink to='/login'>
+                                <button className="inline-block rounded text-red-400 hover:bg-zinc-800 py-1 px-3 hover:text-white" onClick={() => setDropdownOpen(false)}>
+                                    Login
+                                </button>
+                            </NavLink>
+                            <NavLink to='/register'>
+                                <button className="inline-block bg-red-700 rounded hover:border-gray-200 text-white hover:bg-red-900 py-1 px-3">
+                                    Sign up
+                                </button>
+                            </NavLink>
+                        </div>
+                    )}
+                    {auth.user && (
+                        <div>
+                            <div className="flex items-center">
+                                <SearchBar/>
+                                <button
+                                    className="inline-block bg-gray-700 rounded hover:border-gray-200 text-white hover:bg-gray-900 py-2 px-4"
+                                    onClick={() => setDropdownOpen(!dropdownOpen)}
+                                >
+                                    <VscAccount />
+                                </button>
+                            </div>
+                            
+                            {dropdownOpen && (
+                                <div className="absolute right-0 mt-2 w-48 bg-white border border-gray-300 rounded-md shadow-lg">
+                                    <NavLink to='/tickets'>
+                                        <button
+                                            className="rounded block w-full px-4 py-2 text-left text-sm text-gray-700 hover:bg-gray-100"
+                                            onClick={() => setDropdownOpen(false)}
+                                        >
+                                            <div>
+                                                My Tickets
+                                            </div>
+                                        </button>
+                                    </NavLink>
+                                    <button
+                                        className="rounded block w-full px-4 py-2 text-left text-sm text-gray-700 hover:bg-gray-100"
+                                        onClick={handleLogout}
+                                    >
+                                        <div className="flex items-center">
+                                            <CgLogOut className="mr-2" />
+                                            Logout
+                                        </div>
+                                        
+                                    </button>
+                                </div>
+                            )}
+                        </div>
+                    )}
+                </nav>
+            ) : (
+                // Mobile Navbar code
+                <div>
+                    <div className="flex items-center">
+                        <FaBars onClick={handleBarsIconClick} className="text-white cursor-pointer m-4" />
+                        <SearchBar/>
+                    </div>
+                    
+                    {showModal && (
+                        <div className="flex flex-col">
+                            <NavLink to='/'>
+                                <button className="inline-block rounded text-red-400 hover:bg-zinc-800 py-1 px-3 hover:text-white" onClick={() => setDropdownOpen(false)}>
+                                    Home
+                                </button>
+                            </NavLink>
+                            <NavLink to='/movies'>
+                                <button className="inline-block rounded text-red-400 hover:bg-zinc-800 py-1 px-3 hover:text-white" onClick={() => setDropdownOpen(false)}>
+                                    Movies
+                                </button>
+                            </NavLink>
+                            <NavLink to='/movie-theatres'>
+                                <button className="inline-block rounded text-red-400 hover:bg-zinc-800 py-1 px-3 hover:text-white" onClick={() => setDropdownOpen(false)}>
+                                    Movie Theatres
+                                </button>
+                            </NavLink>
+                            <NavLink to='/book-tickets'>
+                                <button className="inline-block rounded text-red-400 hover:bg-zinc-800 py-1 px-3 hover:text-white" onClick={() => setDropdownOpen(false)}>
+                                    Book Tickets
+                                </button>
+                            </NavLink>
+                            {auth.user && isAdmin ? (
+                                <NavLink to='/admin'>
+                                    <button className="inline-block rounded text-red-400 hover:bg-zinc-800 py-1 px-3 hover:text-white" onClick={() => setDropdownOpen(false)}>
+                                        Panel
+                                    </button>
+                                </NavLink>
+                            ): (
+                                <div></div>
+                            )}
+                            {!auth.user ? (
+                                <div className="flex flex-col">
+                                    <NavLink to='/admin-register'>
+                                        <button className="inline-block rounded text-red-400 hover:bg-zinc-800 py-1 px-3 hover:text-white" onClick={() => setDropdownOpen(false)}>
+                                            Admin
+                                        </button>
+                                    </NavLink>
+                                    <NavLink to='/login'>
+                                        <button className="inline-block rounded text-red-400 hover:bg-zinc-800 py-1 px-3 hover:text-white" onClick={() => setDropdownOpen(false)}>
+                                            Login
+                                        </button>
+                                    </NavLink>
+                                    <NavLink to='/register'>
+                                        <button className="inline-block bg-red-700 rounded hover:border-gray-200 text-white hover:bg-red-900 py-1 px-3">
+                                            Sign up
+                                        </button>
+                                    </NavLink>
+                                </div>
+                            ) : (
+                                <div>
+                                    <NavLink to='/tickets'>
+                                        <button
+                                            className="inline-block rounded text-red-400 hover:bg-zinc-800 py-1 px-3 hover:text-white"
+                                            onClick={() => setDropdownOpen(false)}
+                                        >
+                                            <div>
+                                                My Tickets
+                                            </div>
+                                        </button>
+                                    </NavLink>
+                                        <button
+                                            className="inline-block rounded text-red-400 hover:bg-zinc-800 py-1 px-3 hover:text-white"
+                                            onClick={handleLogout}
+                                        >
+                                            <div className="flex items-center">
+                                                <CgLogOut className="mr-2" />
+                                                Logout
+                                            </div>  
+                                        </button>
+                                </div>
+                            )}
+                        </div>
+                    )}
+                </div>
+            )}
         </>
-        
     );
 }
