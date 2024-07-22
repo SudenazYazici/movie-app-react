5501923e31c7220534eb39de59aabc060f92a3de
SudenazYazici <sudenyazici@gmail.com>
2024-07-22 11:33:57 +0300

    responsive navbar styled

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -57,28 +57,28 @@ export const Navbar = () => {
                 <nav className="absolute w-full p-4 flex items-center justify-between z-50">
                     <div>
                         <NavLink to='/'>
-                            <button className="inline-block rounded text-red-400 hover:bg-zinc-800 py-1 px-3 hover:text-white" onClick={() => setDropdownOpen(false)}>
+                            <button className="inline-block rounded text-orange-400 hover:bg-zinc-800 py-1 px-3 hover:text-white" onClick={() => setDropdownOpen(false)}>
                                 Home
                             </button>
                         </NavLink>
                         <NavLink to='/movies'>
-                            <button className="inline-block rounded text-red-400 hover:bg-zinc-800 py-1 px-3 hover:text-white" onClick={() => setDropdownOpen(false)}>
+                            <button className="inline-block rounded text-orange-400 hover:bg-zinc-800 py-1 px-3 hover:text-white" onClick={() => setDropdownOpen(false)}>
                                 Movies
                             </button>
                         </NavLink>
                         <NavLink to='/movie-theatres'>
-                            <button className="inline-block rounded text-red-400 hover:bg-zinc-800 py-1 px-3 hover:text-white" onClick={() => setDropdownOpen(false)}>
+                            <button className="inline-block rounded text-orange-400 hover:bg-zinc-800 py-1 px-3 hover:text-white" onClick={() => setDropdownOpen(false)}>
                                 Movie Theatres
                             </button>
                         </NavLink>
                         <NavLink to='/book-tickets'>
-                            <button className="inline-block rounded text-red-400 hover:bg-zinc-800 py-1 px-3 hover:text-white" onClick={() => setDropdownOpen(false)}>
+                            <button className="inline-block rounded text-orange-400 hover:bg-zinc-800 py-1 px-3 hover:text-white" onClick={() => setDropdownOpen(false)}>
                                 Book Tickets
                             </button>
                         </NavLink>
                         {auth.user && isAdmin ? (
                             <NavLink to='/admin'>
-                                <button className="inline-block rounded text-red-400 hover:bg-zinc-800 py-1 px-3 hover:text-white" onClick={() => setDropdownOpen(false)}>
+                                <button className="inline-block rounded text-orange-400 hover:bg-zinc-800 py-1 px-3 hover:text-white" onClick={() => setDropdownOpen(false)}>
                                     Panel
                                 </button>
                             </NavLink>
@@ -90,17 +90,17 @@ export const Navbar = () => {
                         <div className="flex items-center">
                             <SearchBar/>
                             <NavLink to='/admin-register'>
-                                <button className="inline-block rounded text-red-400 hover:bg-zinc-800 py-1 px-3 hover:text-white" onClick={() => setDropdownOpen(false)}>
+                                <button className="inline-block rounded text-orange-400 hover:bg-zinc-800 py-1 px-3 hover:text-white" onClick={() => setDropdownOpen(false)}>
                                     Admin
                                 </button>
                             </NavLink>
                             <NavLink to='/login'>
-                                <button className="inline-block rounded text-red-400 hover:bg-zinc-800 py-1 px-3 hover:text-white" onClick={() => setDropdownOpen(false)}>
+                                <button className="inline-block rounded text-orange-400 hover:bg-zinc-800 py-1 px-3 hover:text-white" onClick={() => setDropdownOpen(false)}>
                                     Login
                                 </button>
                             </NavLink>
                             <NavLink to='/register'>
-                                <button className="inline-block bg-red-700 rounded hover:border-gray-200 text-white hover:bg-red-900 py-1 px-3">
+                                <button className="inline-block bg-orange-700 rounded hover:border-gray-200 text-white hover:bg-orange-900 py-1 px-3">
                                     Sign up
                                 </button>
                             </NavLink>
@@ -147,37 +147,37 @@ export const Navbar = () => {
                 </nav>
             ) : (
                 // Mobile Navbar code
-                <div>
+                <div className="bg-zinc-800">
                     <div className="flex items-center">
                         <FaBars onClick={handleBarsIconClick} className="text-white cursor-pointer m-4" />
                         <SearchBar/>
                     </div>
                     
                     {showModal && (
-                        <div className="flex flex-col">
+                        <div className="flex flex-col bg-zinc-700">
                             <NavLink to='/'>
-                                <button className="inline-block rounded text-red-400 hover:bg-zinc-800 py-1 px-3 hover:text-white" onClick={() => setDropdownOpen(false)}>
+                                <button className="inline-block rounded text-orange-400 hover:bg-zinc-800 py-1 px-3 hover:text-white" onClick={() => setDropdownOpen(false)}>
                                     Home
                                 </button>
                             </NavLink>
                             <NavLink to='/movies'>
-                                <button className="inline-block rounded text-red-400 hover:bg-zinc-800 py-1 px-3 hover:text-white" onClick={() => setDropdownOpen(false)}>
+                                <button className="inline-block rounded text-orange-400 hover:bg-zinc-800 py-1 px-3 hover:text-white" onClick={() => setDropdownOpen(false)}>
                                     Movies
                                 </button>
                             </NavLink>
                             <NavLink to='/movie-theatres'>
-                                <button className="inline-block rounded text-red-400 hover:bg-zinc-800 py-1 px-3 hover:text-white" onClick={() => setDropdownOpen(false)}>
+                                <button className="inline-block rounded text-orange-400 hover:bg-zinc-800 py-1 px-3 hover:text-white" onClick={() => setDropdownOpen(false)}>
                                     Movie Theatres
                                 </button>
                             </NavLink>
                             <NavLink to='/book-tickets'>
-                                <button className="inline-block rounded text-red-400 hover:bg-zinc-800 py-1 px-3 hover:text-white" onClick={() => setDropdownOpen(false)}>
+                                <button className="inline-block rounded text-orange-400 hover:bg-zinc-800 py-1 px-3 hover:text-white" onClick={() => setDropdownOpen(false)}>
                                     Book Tickets
                                 </button>
                             </NavLink>
                             {auth.user && isAdmin ? (
                                 <NavLink to='/admin'>
-                                    <button className="inline-block rounded text-red-400 hover:bg-zinc-800 py-1 px-3 hover:text-white" onClick={() => setDropdownOpen(false)}>
+                                    <button className="inline-block rounded text-orange-400 hover:bg-zinc-800 py-1 px-3 hover:text-white" onClick={() => setDropdownOpen(false)}>
                                         Panel
                                     </button>
                                 </NavLink>
@@ -187,17 +187,17 @@ export const Navbar = () => {
                             {!auth.user ? (
                                 <div className="flex flex-col">
                                     <NavLink to='/admin-register'>
-                                        <button className="inline-block rounded text-red-400 hover:bg-zinc-800 py-1 px-3 hover:text-white" onClick={() => setDropdownOpen(false)}>
+                                        <button className="inline-block rounded text-orange-400 hover:bg-zinc-800 py-1 px-3 hover:text-white" onClick={() => setDropdownOpen(false)}>
                                             Admin
                                         </button>
                                     </NavLink>
                                     <NavLink to='/login'>
-                                        <button className="inline-block rounded text-red-400 hover:bg-zinc-800 py-1 px-3 hover:text-white" onClick={() => setDropdownOpen(false)}>
+                                        <button className="inline-block rounded text-orange-400 hover:bg-zinc-800 py-1 px-3 hover:text-white" onClick={() => setDropdownOpen(false)}>
                                             Login
                                         </button>
                                     </NavLink>
                                     <NavLink to='/register'>
-                                        <button className="inline-block bg-red-700 rounded hover:border-gray-200 text-white hover:bg-red-900 py-1 px-3">
+                                        <button className="inline-block bg-orange-700 rounded hover:border-gray-200 text-white hover:bg-orange-900 py-1 px-3">
                                             Sign up
                                         </button>
                                     </NavLink>
@@ -206,7 +206,7 @@ export const Navbar = () => {
                                 <div>
                                     <NavLink to='/tickets'>
                                         <button
-                                            className="inline-block rounded text-red-400 hover:bg-zinc-800 py-1 px-3 hover:text-white"
+                                            className="inline-block rounded text-orange-400 hover:bg-zinc-800 py-1 px-3 hover:text-white"
                                             onClick={() => setDropdownOpen(false)}
                                         >
                                             <div>
@@ -215,7 +215,7 @@ export const Navbar = () => {
                                         </button>
                                     </NavLink>
                                         <button
-                                            className="inline-block rounded text-red-400 hover:bg-zinc-800 py-1 px-3 hover:text-white"
+                                            className="inline-block rounded text-orange-400 hover:bg-zinc-800 py-1 px-3 hover:text-white"
                                             onClick={handleLogout}
                                         >
                                             <div className="flex items-center">
