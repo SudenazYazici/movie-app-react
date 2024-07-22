a158159bccf35c0e34a48931916520a7235adb55
SudenazYazici <sudenyazici@gmail.com>
2024-07-22 13:50:52 +0300

    small design fixes

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,8 +4,10 @@ import { MovieTheatres } from "./MovieTheatres";
 export const Home = () => {
     return(
         <>
-            <div className="m-10 font-extrabold">
-                Welcome to Movie Theatre App!
+            <div className="m-10 font-extrabold text-5xl">
+                <span className="bg-clip-text text-transparent bg-gradient-to-r from-orange-300 to-red-900">
+                    Welcome to Movie Theatre App!
+                </span>
             </div>
             <Movies/>
             <MovieTheatres/>
